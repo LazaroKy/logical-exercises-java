5dd19621a6f49f53ddcf03cd93d1a9621524ff73
LázaroK <lazarolk23@hotmail.com>
2025-05-23 22:38:03 -0300

    feat: solve list basic 01 exercise

---
+++ lazaro-exercises/01-basic-list/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> listString = new ArrayList<>();
+
+        listString.add("João");
+        listString.add("Maria");
+        listString.add("Bia");
+        listString.add("Ana");
+        listString.add("Pedro");
+
+        System.out.println(listString);
+    }
+}
